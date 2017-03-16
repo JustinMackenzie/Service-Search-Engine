fd27dcda12c5ffda103b6c11ca61f213217a7eb6
Alex Gyori <alexandergyori@gmail.com>
2017-03-15 23:38:57 -0400

    Implemented GET all, GET, POST, PUT, DELETE

---
+++ Organization-Register/app.js
@@ -0,0 +1,327 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+
+var db;
+
+var cloudant;
+
+var fileToUpload;
+
+var dbCredentials = {
+    dbName: 'organization_registration_db'
+};
+
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
+var multipart = require('connect-multiparty')
+var multipartMiddleware = multipart();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/style', express.static(path.join(__dirname, '/views/style')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(errorHandler());
+}
+
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+    // Pattern match to find the first instance of a Cloudant service in
+    // VCAP_SERVICES. If you know your service key, you can access the
+    // service credentials directly by using the vcapServices object.
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cloudant/i)) {
+            return vcapServices[vcapService][0].credentials.url;
+        }
+    }
+}
+
+function initDBConnection() {
+    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { //When running locally, the VCAP_SERVICES will not be set
+
+        // When running this app locally you can get your Cloudant credentials
+        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+        // Variables section for an app in the Bluemix console dashboard).
+        // Once you have the credentials, paste them into a file called vcap-local.json.
+        // Alternately you could point to a local database here instead of a
+        // Bluemix service.
+        // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
+        dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+    }
+
+    cloudant = require('cloudant')(dbCredentials.url);
+
+    // check if DB exists if not create
+    cloudant.db.create(dbCredentials.dbName, function(err, res) {
+        if (!err) {
+            console.log('Created new db: ' + dbCredentials.dbName)
+        }
+        //if (err) {
+        //    console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
+        //}
+    });
+
+    db = cloudant.use(dbCredentials.dbName);
+}
+
+initDBConnection();
+
+app.get('/', routes.index);
+
+function createResponseData(id, name, description, attachments) {
+
+    var responseData = {
+        id: id,
+        name: sanitizeInput(name),
+        description: sanitizeInput(description),
+        attachements: []
+    };
+
+
+    attachments.forEach(function(item, index) {
+        var attachmentData = {
+            content_type: item.type,
+            key: item.key,
+            url: '/api/organization-register/attach?id=' + id + '&key=' + item.key
+        };
+        responseData.attachements.push(attachmentData);
+
+    });
+    return responseData;
+}
+
+function sanitizeInput(str) {
+    return String(str).replace(/&(?!amp;|lt;|gt;)/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+
+var saveOrganization = function(id, name, description, response) {
+
+    if (id === undefined) {
+        // Generated random id
+        id = '';
+    }
+
+    db.insert({
+        name: name,
+        description: description
+    }, id, function(err, doc) {
+        if (err) {
+            console.log(err);
+            response.sendStatus(500);
+        } else
+            response.sendStatus(200);
+        response.end();
+    });
+
+}
+
+app.post('/api/organization-register', function (request, response) {
+
+    console.log("Create Invoked..");
+    console.log("Name: " + request.body.name);
+    console.log("Description: " + request.body.description);
+
+    // var id = request.body.id;
+    var name = sanitizeInput(request.body.name);
+    var description = sanitizeInput(request.body.description);
+
+    saveOrganization(null, name, description, response);
+
+});
+
+app.delete('/api/organization-register', function (request, response) {
+
+    console.log("Delete Invoked..");
+    var id = request.query.id;
+    // var rev = request.query.rev; // Rev can be fetched from request. if
+    // needed, send the rev from client
+    console.log("Removing document of ID: " + id);
+    console.log('Request Query: ' + JSON.stringify(request.query));
+
+    db.get(id, {
+        revs_info: true
+    }, function(err, doc) {
+        if (!err) {
+            db.destroy(doc._id, doc._rev, function(err, res) {
+                // Handle response
+                if (err) {
+                    console.log(err);
+                    response.sendStatus(500);
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        }
+    });
+
+});
+
+app.put('/api/organization-register', function (request, response) {
+
+    console.log("Update Invoked..");
+
+    var id = request.body.id;
+    var name = sanitizeInput(request.body.name);
+    var description = sanitizeInput(request.body.description);
+
+    console.log("ID: " + id);
+
+    db.get(id, {
+        revs_info: true
+    }, function(err, doc) {
+        if (!err) {
+            console.log(doc);
+            doc.name = name;
+            doc.description = description;
+            db.insert(doc, function(err, doc) {
+                if (err) {
+                    console.log('Error inserting data\n' + err);
+                    response.sendStatus(500);
+                } else {
+                    response.sendStatus(200);
+                }
+            });
+        } else {
+            response.sendStatus(500);
+        }
+    });
+});
+
+app.get('/api/organization-register', function (request, response) {
+
+    console.log("Get method invoked...");
+
+    var id = request.query.id;
+
+    db.get(id, {
+        revs_info: false
+    }, function (err, doc) {
+        if (!err) {
+            response.send(doc);
+        }
+    });
+});
+
+app.get('/api/organization-register/getAllOrgs', function (request, response) {
+
+    console.log("Get All Orgs method invoked.. ")
+
+    db = cloudant.use(dbCredentials.dbName);
+    var docList = [];
+    var i = 0;
+    db.list(function(err, body) {
+        if (!err) {
+            var len = body.rows.length;
+            console.log('total # of docs -> ' + len);
+            if (len == 0) {
+                // push sample data
+                // save doc
+                var docName = 'sample_doc';
+                var docDesc = 'A sample Document';
+                db.insert({
+                    name: docName,
+                    value: 'A sample Document'
+                }, '', function(err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+
+                        console.log('Document : ' + JSON.stringify(doc));
+                        var responseData = createResponseData(
+                            doc.id,
+                            docName,
+                            docDesc, []);
+                        docList.push(responseData);
+                        response.write(JSON.stringify(docList));
+                        console.log(JSON.stringify(docList));
+                        console.log('ending response...');
+                        response.end();
+                    }
+                });
+            } else {
+
+                body.rows.forEach(function(document) {
+
+                    db.get(document.id, {
+                        revs_info: true
+                    }, function(err, doc) {
+                        if (!err) {
+                            if (doc['_attachments']) {
+
+                                var attachments = [];
+                                for (var attribute in doc['_attachments']) {
+
+                                    if (doc['_attachments'][attribute] && doc['_attachments'][attribute]['content_type']) {
+                                        attachments.push({
+                                            "key": attribute,
+                                            "type": doc['_attachments'][attribute]['content_type']
+                                        });
+                                    }
+                                    console.log(attribute + ": " + JSON.stringify(doc['_attachments'][attribute]));
+                                }
+                                var responseData = createResponseData(
+                                    doc._id,
+                                    doc.name,
+                                    doc.description,
+                                    attachments);
+
+                            } else {
+                                var responseData = createResponseData(
+                                    doc._id,
+                                    doc.name,
+                                    doc.description, []);
+                            }
+
+                            docList.push(responseData);
+                            i++;
+                            if (i >= len) {
+                                response.write(JSON.stringify(docList));
+                                console.log('ending response...');
+                                response.end();
+                            }
+                        } else {
+                            console.log(err);
+                        }
+                    });
+
+                });
+            }
+
+        } else {
+            console.log(err);
+        }
+    });
+
+});
+
+
+http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
